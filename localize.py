3032425bd412e7b450ebcf19a66b017651573a12
Anoop Bhat <noopy.g.bhat@gmail.com>
2021-03-31 05:55:28 +0100

    Reduce frequency of weight updates

--- localize.py
+++ localize.py
@@ -7,6 +7,7 @@ from kf import *
 mcu = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 initialized = False
 prev_yaw = 0
+yaw_chkpt = 0
 
 nfilters = 8
 states = np.zeros((5, nfilters))
@@ -26,6 +27,7 @@ while True:
 
       if not initialized:
         prev_yaw = data[0]
+        yaw_chkpt = data[0]
 
         for i in range(nfilters):
           states[:, i], covs[i] = init_state_given_yaw(i*np.pi/4, data[1:])
@@ -37,18 +39,22 @@ while True:
       t = time.time()
       yaw = data[0]
 
+      new_log_weights = np.zeros(nfilters)
       for i in range(nfilters):
         states[:, i], covs[i] = predict(states[:, i], covs[i], yaw - prev_yaw, t - prev_t)
-        states[:, i], covs[i], log_weights[i] = correct(states[:, i], covs[i], data[1:], log_weights[i])
+        states[:, i], covs[i], new_log_weights[i] = correct(states[:, i], covs[i], data[1:], log_weights[i])
 
-      log_weights = normalize_log_weights(log_weights)
+      if abs(yaw - yaw_chkpt) > np.pi/2:
+        yaw_chkpt = yaw
+        log_weights = new_log_weights
+        log_weights = normalize_log_weights(log_weights)
 
-      live_filters = log_weights > -10
-      states = states[:, live_filters]
-      covs = covs[live_filters]
-      log_weights = normalize_log_weights(log_weights[live_filters])
-      nfilters = len(covs)
-
+        live_filters = np.logical_and(np.logical_and(log_weights > -10, states[0] > 0), states[1] > 0)
+        states = states[:, live_filters]
+        covs = covs[live_filters]
+        log_weights = normalize_log_weights(log_weights[live_filters])
+        nfilters = len(covs)
+        
       state = np.matmul(states, np.exp(log_weights))
       state_deg = np.copy(state)
       state_deg[2] *= 180/np.pi
